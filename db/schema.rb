94dadff61b529eb89565c48302c2763c5049bcb0
Lloyd Nugent Gibson <lnugentgibson@gmail.com>
2015-01-25 03:35:48 +0000

    gitignore again

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150125010950) do
+ActiveRecord::Schema.define(version: 20150125011825) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "ic"
+    t.text     "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer  "product_id"
+    t.date     "date"
+    t.integer  "type"
+    t.string   "north"
+    t.string   "north_east"
+    t.string   "north_west"
+    t.string   "south"
+    t.string   "south_east"
+    t.string   "south_west"
+    t.string   "east"
+    t.string   "west"
+    t.string   "image1"
+    t.string   "image2"
+    t.string   "image3"
+    t.string   "image4"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
