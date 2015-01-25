3bdeaa312446bf29985f154ab744b10a8084eb0d
Rabeet Fatmi <raf6000@gmail.com>
2015-01-25 05:36:16 +0000

    New Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,9 +10,21 @@ class ProductsController < ApplicationController
   end
   
   def new
+    @product = Product.new
   end
   
   def create
+    @product = Product.new(post_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
   end
   
   def edit
